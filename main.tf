62a2725d2b90686f8d7e209d52b61dc3c4f8f33e
Michaelsliem <31589154+Michaelsliem@users.noreply.github.com>
2019-05-20 11:38:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,31 @@
-#
+provider "google" {
+  project    = "sharp-nation-214017"
+  credentials = "${file("/tmp/account.json")}"
+}
+
+resource "google_compute_router" "router1" {
+  name    = "ic-router"
+  network = "${google_compute_network.network1.name}"
+  bgp {
+    asn               = 64514
+    advertise_mode    = "CUSTOM"
+    advertised_groups = ["ALL_SUBNETS"]
+    advertised_ip_ranges {
+      range = "1.2.3.4"
+    }
+    advertised_ip_ranges {
+      range = "6.7.0.0/16"
+    }
+  }
+}
+resource "google_compute_interconnect_attachment" "on_prem" {
+  name         = "on-prem-attachment-1"
+  interconnect = "my-interconnect-id"
+  router       = "${google_compute_router.router1.self_link}"
+}
+
+resource "google_compute_network" "network1" {
+  name = "default"
+  auto_create_subnetworks = false
+}
+
